3c76cd21e005285ce8b694b25296de9b5c773735
Steve Idowu <idowusteve@ymail.com>
2020-05-23 14:36:33 +0100

    Setting up configuration, environment variables and debugger

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const express = require('express')
-const Joi = require('@hapi/joi')
-const genre = require('./controllers/genre')
-const app = express()
+const startUpDebugger = require("debug")("vidly:startup");
+const dbDebugger = require("debug")("vidly:db");
+const config = require("config");
+const express = require("express");
+const morgan = require("morgan");
+const Joi = require("@hapi/joi");
+const genre = require("./controllers/genre");
 
-app.use(express.json())
+const app = express();
 
-app.get('/api/generes', genre.index)
-app.get('/api/generes/:id', genre.show)
-app.post('/api/generes', genre.store)
-app.put('/api/generes/:id', genre.update)
-app.delete('/api/generes/:id', genre.destory)
+app.use(express.json());
 
-const port = process.env.PORT || 3000
+//console.log('Application name: ' + config.get('name'))
+// console.log(config.get('db_name'))
 
-app.listen(port, () => console.log(`Node develoment server started at http://localhost${port}`))
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  startUpDebugger("Morgan running...");
+}
+dbDebugger('connecting to database...');
+app.get("/api/generes", genre.index);
+app.get("/api/generes/:id", genre.show);
+app.post("/api/generes", genre.store);
+app.put("/api/generes/:id", genre.update);
+app.delete("/api/generes/:id", genre.destory);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  startUpDebugger(`Node develoment server started at http://localhost:${port}`)
+);
